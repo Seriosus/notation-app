f360fd632001a17ed55d1f4e466ee203961d0c19
Alekz <maximalekz@hotmail.com>
2025-08-01 18:39:35 -0500

    Removing colors to allow system put automatic text colors

--- app/src/main/java/com/lyra/notation/BaseActivity.kt
+++ app/src/main/java/com/lyra/notation/BaseActivity.kt
@@ -1,13 +1,9 @@
 package com.lyra.notation
 
 import android.os.Bundle
-import android.view.View
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updateLayoutParams
 
 abstract class BaseActivity(@LayoutRes private val layoutRes: Int): AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,19 +11,6 @@ abstract class BaseActivity(@LayoutRes private val layoutRes: Int): AppCompatAct
         supportActionBar?.hide()
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(layoutRes)
-    }
-
-    private fun setupInsetSpacer() {
-        val spacer = findViewById<View?>(R.id.statusBarSpacer) ?: return
-
-        ViewCompat.setOnApplyWindowInsetsListener(spacer) {
-            view, insets ->
-                val top = insets.getInsets(WindowInsetsCompat.Type.systemBars()).top
-                view.updateLayoutParams {
-                    height = top
-                }
-                insets
-        }
     }
 
     fun isPortrait(): Boolean {
